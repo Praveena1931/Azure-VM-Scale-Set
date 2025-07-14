9a808306f8137bc3b73adfe7921b78dc701c6d2c
Praveena <praveena.y@algorims.com>
2025-07-14 16:46:42 +0530

    VMSS INFRACOST

--- terraform/modules/backend.tf
+++ terraform/modules/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg1"      
     storage_account_name = "tfstatestorageacct11"      
-    container_name       = "tfstate"                 
+    container_name       = "tfstate1"                 
     key                  = "terraform.tfstate"  
   }
 }
