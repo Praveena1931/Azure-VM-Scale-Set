9d90478921948dc619fc6a12eed7cbdafbf24164
Praveena <praveena.y@algorims.com>
2025-07-14 16:20:10 +0530

    VMSS INFRACOST

--- terraform/modules/provider.tf
+++ terraform/modules/provider.tf
@@ -2,10 +2,10 @@ provider "azurerm" {
   features {} 
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
-}
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = "East US"
+# }
 
 terraform {
   required_providers {
