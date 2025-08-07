26f9ed9b0efdce38e8022860ac5f2f590d46a90b
Praveena <praveena.y@algorims.com>
2025-08-07 15:05:10 +0530

    VMSS

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg1"      
     storage_account_name = "tfstatestorageacct11"      
-    container_name       = "tfstate11"                 
+    container_name       = "tfstate111"                 
     key                  = "VMSS2.tfstate"  
   }
 }
